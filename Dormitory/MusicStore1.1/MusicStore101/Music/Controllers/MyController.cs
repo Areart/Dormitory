fe49994a18c8ab0d021679edd49d17167b0e863e
Areart <15778250484@163.com>
2018-12-14 16:19:21 +0800

    2018-12-14

--- Dormitory/MusicStore1.1/MusicStore101/Music/Controllers/MyController.cs
+++ Dormitory/MusicStore1.1/MusicStore101/Music/Controllers/MyController.cs
@@ -12,6 +12,13 @@ namespace Music.Controllers
     public class MyController : Controller
     {
         private static readonly EntityDbContext _context = new EntityDbContext();
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public ActionResult Add(PersonAddress personAddress)
         {
             //1.确认用户是否登陆 是否登陆过期
@@ -22,8 +29,8 @@ namespace Music.Controllers
             }
             //2.查询出当前用户Person 查询该用户的购物项
             var persons = (Session["loginUserSessionModel"] as LoginUserSessionModel).Person;
-            var person = _context.Persons.Find(persons.ID);
-            person.PersonAddresss = new List<PersonAddress>();
+           
+            var person = _context.Persons.SingleOrDefault(x=>x.ID==persons.ID);
             if (personAddress.AddresPerson != null)
             {
                 person.PersonAddresss.Add(personAddress);
@@ -32,10 +39,32 @@ namespace Music.Controllers
             
             return View();
         }
-        public ActionResult Remove()
+        [HttpPost]
+        public ActionResult Remove(Guid id)
         {
+            //1.确认用户是否登陆 是否登陆过期
+            if (Session["loginUserSessionModel"] == null)
+            {
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Buy", "Order") });
 
-            return View();
+            }
+            var persons = (Session["loginUserSessionModel"] as LoginUserSessionModel).Person;
+            var addres = _context.PersonAddresss.Find(id);
+            _context.PersonAddresss.Remove(addres);
+            _context.SaveChanges();
+
+            var address = _context.Persons.Find(persons.ID).PersonAddresss.ToList();
+            var htmlString = "";
+            foreach (var item in address)
+            {
+                htmlString += "<tr>";
+                htmlString += "<td style=\"width:250px\">"+item.AddresPerson+"</td>";
+                htmlString += "<td style=\"width:400px\">"+item.Address+"</td>";
+                htmlString += "<td style=\"width:100px\">"+item.MobileNumber+"</td>";
+                htmlString += "<td style=\"width:100px\"><button onclick=\"removeAddres("+item.ID+")\" class=\"btn btn-success\"><i class=\"glyphicon glyphicon-shopping - cart\"></i>删除</button></td>";
+                htmlString += "</tr>";
+            }
+            return Json(htmlString);
         }
         // GET: My
         public ActionResult Index()
