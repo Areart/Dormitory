def0d96c32469ce69e90a481e2c16a65a876b428
Areart <15778250484@163.com>
2018-12-16 16:32:40 +0800

    2018-12-16

--- Dormitory/MusicStore1.1/MusicStore101/Music/Controllers/MyController.cs
+++ Dormitory/MusicStore1.1/MusicStore101/Music/Controllers/MyController.cs
@@ -81,6 +81,10 @@ namespace Music.Controllers
          
             return View(address);
         }
+        /// <summary>
+        /// 查看个人信息
+        /// </summary>
+        /// <returns></returns>
         public ActionResult MyIndex()
         {
             //1.确认用户是否登陆 是否登陆过期
@@ -90,8 +94,33 @@ namespace Music.Controllers
 
             }
             var persons = (Session["loginUserSessionModel"] as LoginUserSessionModel).Person;
-            
+            return View(persons);
+        }
+        /// <summary>
+        /// 修改个人信息
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult UpMy()
+        {
+            //1.确认用户是否登陆 是否登陆过期
+            if (Session["loginUserSessionModel"] == null)
+            {
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Buy", "Order") });
 
+            }
+            var persons = (Session["loginUserSessionModel"] as LoginUserSessionModel).Person;
+            return View(persons);
+        }
+        [HttpPost]
+        public ActionResult UpMy(Guid id)
+        {
+            //1.确认用户是否登陆 是否登陆过期
+            if (Session["loginUserSessionModel"] == null)
+            {
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Buy", "Order") });
+
+            }
+            var persons = (Session["loginUserSessionModel"] as LoginUserSessionModel).Person;
             return View(persons);
         }
     }
