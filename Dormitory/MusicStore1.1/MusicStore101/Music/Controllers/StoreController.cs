6ac07ded18fcc567f9d09e30a69394ff48680592
Areart <15778250484@163.com>
2018-12-30 10:13:24 +0800

    2018-12-30

--- Dormitory/MusicStore1.1/MusicStore101/Music/Controllers/StoreController.cs
+++ Dormitory/MusicStore1.1/MusicStore101/Music/Controllers/StoreController.cs
@@ -30,7 +30,7 @@ namespace Music.Controllers
                 htmlString += "<div id='pinl_main'>";//最外层
                 htmlString += "<div id='pinl_main_main'>";//第一层
                 htmlString += "<div>";//第一层中的图片
-                htmlString += "<img src=" + item.Person.Avarda + ">";
+                htmlString += "<img onload='imgWidth(this)' src=" + item.Person.Avarda + ">";
                 htmlString += "</div>";//图片结束
                 htmlString += "<div>";//评论内容
                 htmlString += "<p>" + item.Title + "</p>";
@@ -39,11 +39,11 @@ namespace Music.Controllers
                 htmlString += "<div>";//第一层中的功能栏
                 htmlString += "<ul id='"+item.ID+"'>";
                 htmlString += "<li>#"+louc+"</li>";
-                htmlString += "<li>"+item.CreateDateTime+"</li>";
+                htmlString += "<li>"+item.CreateDateTime.ToString("yyyy年MM月hh日")+"</li>";
                 htmlString += "<li onclick='ADD(this)'>回复</li>";
                 htmlString += "<li><i class='glyphicon glyphicon-thumbs-up'></i>（" + item.Like + "）</li>";
                 htmlString += "<li><i class='glyphicon glyphicon-thumbs-down'></i>（" + item.Hate + "）</li>";
-                htmlString += "<li onclick='pinlMain(this)'>查看对话内容</li>";
+                htmlString += "<li onclick='pinlMain(this)'>对话内容</li>";
                 htmlString += "</ul>";
                 htmlString += "</div>";//功能栏结束
                 htmlString += "</div>";//第一层结束
@@ -55,7 +55,7 @@ namespace Music.Controllers
                     {
                         htmlString += "<div id='pinl_main_main'>";//第一层
                         htmlString += "<div style='margin-bottom:50px'>";//第一层中的图片
-                        htmlString += "<img style='width:40px;height:40px'  src=" + ry.Person.Avarda + ">";
+                        htmlString += "<img style='width:70%;'  src=" + ry.Person.Avarda + ">";
                         htmlString += "</div>";//图片结束
                         htmlString += "<div>";//评论内容
                         if (_context.Replys.Find(ry.ParentReplyNameID)!= null)
@@ -71,11 +71,11 @@ namespace Music.Controllers
                         htmlString += "<div>";//第一层中的功能栏
                         htmlString += "<ul id='" + ry.ID + "'>";
                         htmlString += "<li></li>";
-                        htmlString += "<li>" + ry.CreateDateTime + "</li>";
+                        htmlString += "<li>" + ry.CreateDateTime.ToString("yyyy年MM月hh日") + "</li>";
                         htmlString += "<li onclick='ADD(this)'>回复</li>";
                         htmlString += "<li><i class='glyphicon glyphicon-thumbs-up'></i>(" + item.Like + ")</li>";
                         htmlString += "<li><i class='glyphicon glyphicon-thumbs-down'></i>(" + item.Hate + ")</li>";
-                        htmlString += "<li>查看对话内容</li>";
+                        htmlString += "<li>对话内容</li>";
                         htmlString += "</ul>";
                         htmlString += "</div>";//功能栏结束
                         htmlString += "</div>";//第一层结束
@@ -83,8 +83,8 @@ namespace Music.Controllers
 
                 }
                 htmlString += "</div>";//第二层回复结束
-                if (prenreply.Where(x => x.ParentReply.ID == item.ID).ToList().Count>1) 
-                htmlString += "<p class='pinl_p' id='a" + louc + "' onclick='openmain(this)'>查看所有回复(" + prenreply.Where(x => x.ParentReply.ID == item.ID).ToList().Count + ")</p>";
+                if (prenreply.Where(x => x.ParentReply.ID == item.ID).ToList().Count>1)
+                    htmlString += "<p class='pinl_p' id='a" + louc + "' onclick='openmain(this)'>共" + prenreply.Where(x => x.ParentReply.ID == item.ID).ToList().Count + "条回复，点击查看</p>";
                 htmlString += "<hr>";
                 htmlString += "</div>";
             }
@@ -132,7 +132,7 @@ namespace Music.Controllers
         /// <param name="contentreply">子回复内容</param>
         /// <returns></returns>
         [HttpPost]
-        [ValidateInput(false)]
+        [ValidateInput(false)]//关闭验证
         public ActionResult Reply(Guid id,Guid replyID, string content,string contentreply)
          {
             //判断用户是否登陆
@@ -181,7 +181,7 @@ namespace Music.Controllers
                 htmlString += "<div id='pinl_main'>";//最外层
                 htmlString += "<div id='pinl_main_main'>";//第一层
                 htmlString += "<div>";//第一层中的图片
-                htmlString += "<img src=" + item.Person.Avarda + ">";
+                htmlString += "<img onload='imgWidth(this)' src=" + item.Person.Avarda + ">";
                 htmlString += "</div>";//图片结束
                 htmlString += "<div>";//评论内容
                 htmlString += "<p>" + item.Title + "</p>";
@@ -190,11 +190,11 @@ namespace Music.Controllers
                 htmlString += "<div>";//第一层中的功能栏
                 htmlString += "<ul id='" + item.ID + "'>";
                 htmlString += "<li>#" + louc + "</li>";
-                htmlString += "<li>" + item.CreateDateTime + "</li>";
+                htmlString += "<li>" + item.CreateDateTime.ToString("yyyy年MM月hh日") + "</li>";
                 htmlString += "<li onclick='ADD(this)'>回复</li>";
                 htmlString += "<li><i class='glyphicon glyphicon-thumbs-up'></i>（" + item.Like + "）</li>";
                 htmlString += "<li><i class='glyphicon glyphicon-thumbs-down'></i>（" + item.Hate + "）</li>";
-                htmlString += "<li onclick='pinlMain(this)'>查看对话内容</li>";
+                htmlString += "<li onclick='pinlMain(this)'>对话内容</li>";
                 htmlString += "</ul>";
                 htmlString += "</div>";//功能栏结束
                 htmlString += "</div>";//第一层结束
@@ -222,11 +222,11 @@ namespace Music.Controllers
                         htmlString += "<div>";//第一层中的功能栏
                         htmlString += "<ul id='" + ry.ID + "'>";
                         htmlString += "<li></li>";
-                        htmlString += "<li>" + ry.CreateDateTime + "</li>";
+                        htmlString += "<li>" + ry.CreateDateTime.ToString("yyyy年MM月hh日")+"</li>";
                         htmlString += "<li onclick='ADD(this)'>回复</li>";
                         htmlString += "<li><i class='glyphicon glyphicon-thumbs-up'></i>(" + item.Like + ")</li>";
                         htmlString += "<li><i class='glyphicon glyphicon-thumbs-down'></i>(" + item.Hate + ")</li>";
-                        htmlString += "<li>查看对话内容</li>";
+                        htmlString += "<li>对话内容</li>";
                         htmlString += "</ul>";
                         htmlString += "</div>";//功能栏结束
                         htmlString += "</div>";//第一层结束
@@ -235,11 +235,45 @@ namespace Music.Controllers
                 }
                 htmlString += "</div>";//第二层回复结束
                 if (prenreply.Where(x => x.ParentReply.ID == item.ID).ToList().Count > 1)
-                    htmlString += "<p class='pinl_p' id='a" + louc + "' onclick='openmain(this)'>查看所有回复(" + prenreply.Where(x => x.ParentReply.ID == item.ID).ToList().Count + ")</p>";
+                    htmlString += "<p class='pinl_p' id='a" + louc + "' onclick='openmain(this)'>共" + prenreply.Where(x => x.ParentReply.ID == item.ID).ToList().Count + "条回复，点击查看</p>";
                 htmlString += "<hr>";
                 htmlString += "</div>";
             }
             return Json(htmlString);
+        }
+
+        /// <summary>
+        /// 点赞
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Like(Guid id)
+        {
+            //判断用户是否登陆
+
+            //判断用户是否队这条回复点过赞或踩
+
+            //保存 Reply实体中like +1 或hate+1 LikeReply添加一条纪录
+
+            //生成html 注入视图
+            return Json("OK");
+        }
+
+        /// <summary>
+        /// 踩
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Hate(Guid id)
+        {
+            //判断用户是否登陆
+
+            //判断用户是否队这条回复点过赞或踩
+
+            //保存 Reply实体中like +1 或hate+1 LikeReply添加一条纪录
+
+            //生成html 注入视图
+            return Json("OK");
         }
     }
 }
